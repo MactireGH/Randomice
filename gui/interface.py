714a9ef5b07a283fc8867fce17b6674bfb357438
Nikita Zolotarev <148555893+MactireGH@users.noreply.github.com>
2025-07-06 21:24:32 +0300

    Adding the abouttab.py

--- gui/interface.py
+++ gui/interface.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 from tkinter import ttk
 from gui.maintab import get_create_main_tab
+from gui.abouttab import get_create_about_tab
 
 def run_app():
     """Функция для сборки пользовательского интерфейса."""
@@ -15,5 +16,8 @@ def run_app():
 
     main_tab, elements = get_create_main_tab(notebook)
     notebook.add(main_tab, text='Главная')
+
+    about_tab = get_create_about_tab(notebook)
+    notebook.add(about_tab, text='О программе')
 
     root.mainloop()
